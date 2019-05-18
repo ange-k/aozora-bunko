b1f3809a8cd82a78ce0240c85414db4041167142
shogo takayama <shogo@chalkboard.me>
2019-05-18 18:17:08 +0900

    コミット漏れ

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				include: [path.resolve(__dirname, 'src')],
+				loader: 'babel-loader',
+
+				options: {
+					plugins: ['syntax-dynamic-import'],
+
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								modules: false
+							}
+						]
+					]
+				},
+
+				test: /\.js$/
+			},
+			{
+				test: /\.(scss|css)$/,
+
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	},
+
+	output: {
+//		chunkFilename: '[name].[chunkhash].js',  // ランダム名の生成。キャッシュのある状態ではこうしないと
+//		filename: '[name].[chunkhash].js',       // CSSやJSが反映されるまで時間がかかる
+		filename: "index.js",
+		path: path.resolve(__dirname, 'docs')
+	},
+
+	mode: 'development',
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	}
+};
